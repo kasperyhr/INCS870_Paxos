4a1b2b5c86a10b0aeb9afe86d73a43491668bfa8
Kasper Yang <kasperyhr@gmail.com>
2024-01-30 01:35:08 -0500

    Create server.java file

---
+++ version1/Server.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+  public static void main(String[] args) {
+    // This is the start of the Server.
+    System.out.println("Hello world!");
+  }
+}
